028e896e10a399064d50e43ea383d4764950dadd
Gibran Lopez Morales <gibran@michelada.io>
2018-10-15 18:45:00 -0500

    production: generate first production test[2]

--- repositories/react-stuffs/build/service-worker.js
+++ repositories/react-stuffs/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.207aef8dae21df9aa8ce48a8e307719f.js"
+  "/repositories/react-stuffs/build/precache-manifest.da8c9d31cd459bd768742a6eb22d30cf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/repositories/react-stuffs/build/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
